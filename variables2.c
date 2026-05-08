5badb34bc64ba5ce41204aa7f1f2666484b2fb94
menyinet <menyi.app@gmail.com>
2026-05-08 19:15:36 +0200

    variables-numericas

---
+++ variables2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+//declaracion de una variable tipo float (numero que puede tener decimales)
+//definir el tipo de dato + nombre de la varaible+ el valor de la variable
+//ENTRADA DE DATOS
+    float altura = 1.55; //valor  de la variable altura
+    float peso = 70.5; //valor de la variable peso
+//procesamiento de datos tratamiento de datos
+    printf("La altura es: %f\n", altura);
+    printf("El peso es: %f\n", peso);
+    printf("La altura es: %f y el peso es: %f", altura, peso);
+//salida de datos
+    return 0;
+}
