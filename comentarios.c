43a95935eaa932aafd2950f85c295e717bda7b18
Antonio Heredia <heran76@gmail.com>
2026-05-08 17:41:27 +0200

    TAREA VERSION 1

---
+++ comentarios.c
@@ -0,0 +1,22 @@
+// ESTE CODGIO ES UN PROGRAMA
+//menyi martin
+//08-05-2026
+//ESTE ARCHIVO se llama comentario.c
+#include <stdio.h>
+
+int main() {
+//entrada de datos
+    // Este programa muestra un mensaje
+// ESTE PROGRAMA EJECUTAR  LA ACCION DE IMPRESION EN PANTALLA
+// tratamiento de datos
+    printf("Mi primer programa CON COMENTARIOS SOY mENYI en C\n");
+//ESTE COMENTARIO ES UNA SOLA LINEA 
+    /*
+       Este comentario
+       ocupa varias líneas
+    */
+//salida de datos
+    printf("Saliendo del programa\n");
+//SI HAGO UN CAMBIO EN EL ARCHIVO ORIGEN / FUENTE HAY QUE VOVLER A COMPILAR
+    return 0;
+}
---
+++ variables1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+// DECLARACION DE una  VARIABLE TIPO int (intenger) es numerica y es un  numero entero sin decimales
+// TIPO DE VARIABLE + NOMBRE DE LA VARIABLE + VALOR DE LA VARIABLE
+//entrada de datos
+    int numeroAlumnos =5; // PUNTO Y COMA PARA FINALIZAR LA SENTENCIA  
+//procesamiento de datos
+//funcion imprimir en pantalla con unna opciojn dentro del texto
+//para sustuitir por el valor de la variable se usa el simbolo % seguido de la letra que corresponde al tipo de dato
+    printf("La edad es: %d\n", numeroAlumnos);
+//salida de datos
+    return 0;
+}
