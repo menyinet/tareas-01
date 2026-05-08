3b78352c3943d21f009cbf8327b493ebf859f68e
menyinet <menyi.app@gmail.com>
2026-05-08 19:32:38 +0200

    ultimos-viernes

---
+++ variables3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // El texto se escribe entre comillas dobles "".
+    // Las comillas simples '' se usan para un solo caracter.
+    char texto[] = "A BCDEFGHIJKLMN OPQRSTUV NNNNNNN AEIOU mmmmm ";
+
+    // texto es el nombre de la variable.
+    // char[] permite almacenar texto, tambien llamado string o cadena de texto.
+    printf("El texto es: %s\n", texto);
+    //tipo string comodinla %s para texto o cadena de texto
+
+    return 0;
+}
